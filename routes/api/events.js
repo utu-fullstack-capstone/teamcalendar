f491f415e455686ceffb7eb96d11351d5cd954f9
Samuel Koivisto <samuel.j.koivisto@gmail.com>
2019-07-02 12:06:17 +0300

    post route for events

--- routes/api/events.js
+++ routes/api/events.js
@@ -1,12 +1,50 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const config = require('config')
+const router = express.Router()
+const { check, validationResult } = require('express-validator')
+const Event = require('../../models/Events.js')
 
-// POST api/events
-// Test route for posting events to mongo
+// POST api/posts
 
-router.post('/', (req, res) => {
-  res.json(req.body);
-  console.log(req.body);
-});
+router.post(
+  '/', 
+  [
+    check('title', 'Title is required')
+      .not()
+      .isEmpty(),
+    check('location', 'Location is required')
+      .not()
+      .isEmpty(),
+    check('from', 'Date is required')
+      .not()
+      .isEmpty()
+  ],
+  async (req, res) => {
 
-module.exports = router;
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+
+      const newEvent = new Event({
+        title: req.body.title,
+        description: req.body.description,
+        location: req.body.location,
+        from: req.body.from,
+        to: req.body.to,
+      })
+
+      const event = await newEvent.save()
+      console.log('event saved!')
+      res.json(event)
+
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+)
+
+module.exports = router
