a5a20db861444ee1939ce6072e67f134f5d1f473
Pasi Hietanen <pasi.hietanen@protonmail.com>
2019-07-03 14:36:49 +0300

    eslint & prettier

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
 function App() {
-  const [message, setData] = useState();
+	const [message, setData] = useState();
 
-  useEffect(() => {
-    async function fetchData() {
-      // You can await here
-      const result = await axios('/api/test/');
-      setData(result.data.message);
-      console.log(result.data.message);
-    }
-    fetchData();
-  }, []);
+	useEffect(() => {
+		async function fetchData() {
+			// You can await here
+			const result = await axios('/api/test/');
+			setData(result.data.message);
+			console.log(result.data.message);
+		}
+		fetchData();
+	}, []);
 
-  return <h1>{message}</h1>;
+	return <h1 className='plapla'>{message}</h1>;
 }
 
 export default App;
