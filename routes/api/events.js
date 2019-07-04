b48610854239556210f132e299c0eb4515238c9f
Samuel Koivisto <samuel.j.koivisto@gmail.com>
2019-07-04 10:36:23 +0300

    event routes

--- routes/api/events.js
+++ routes/api/events.js
@@ -1,50 +1,80 @@
-const express = require('express')
-const config = require('config')
-const router = express.Router()
-const { check, validationResult } = require('express-validator')
-const Event = require('../../models/Events.js')
+const express = require('express');
+const config = require('config');
+const router = express.Router();
+const {check, validationResult} = require('express-validator');
+const Event = require('../../models/Events.js');
 
-// POST api/posts
+// POST event
 
 router.post(
-  '/', 
-  [
-    check('title', 'Title is required')
-      .not()
-      .isEmpty(),
-    check('location', 'Location is required')
-      .not()
-      .isEmpty(),
-    check('from', 'Date is required')
-      .not()
-      .isEmpty()
-  ],
-  async (req, res) => {
+	'/',
+	[
+		check('title', 'Title is required')
+			.not()
+			.isEmpty(),
+		check('location', 'Location is required')
+			.not()
+			.isEmpty(),
+		check('from', 'Date is required')
+			.not()
+			.isEmpty(),
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({errors: errors.array()});
+		}
 
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
+		try {
+			const newEvent = new Event({
+				title: req.body.title,
+				description: req.body.description,
+				location: req.body.location,
+				from: req.body.from,
+				to: req.body.to,
+				category: req.body.category,
+				teams: req.body.teams,
+			});
 
-    try {
+			const event = await newEvent.save();
+			console.log('event saved!');
+			res.json(event);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Server Error');
+		}
+	},
+);
 
-      const newEvent = new Event({
-        title: req.body.title,
-        description: req.body.description,
-        location: req.body.location,
-        from: req.body.from,
-        to: req.body.to,
-      })
+// GET event by id
 
-      const event = await newEvent.save()
-      console.log('event saved!')
-      res.json(event)
+router.get('/:id', async (req, res) => {
+	try {
+		const event = await Event.findById(req.params.id);
 
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send('Server Error');
-    }
-  }
-)
+		if (!event) {
+			return res.status(404).json({msg: 'Event not found'});
+		}
+		res.json(event);
+	} catch (err) {
+		console.error(err.message);
+		if (err.kind === 'ObjectId') {
+			return res.status(404).json({msg: 'Event not found'});
+		}
+		res.status(500).send('Server Error');
+	}
+});
 
-module.exports = router
+// GET all events
+
+router.get('/', async (req, res) => {
+	try {
+		const events = await Event.find();
+		res.json(events);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
+});
+
+module.exports = router;
