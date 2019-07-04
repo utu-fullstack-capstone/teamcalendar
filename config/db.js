d312d36570ebe9915ebc222b4f88884f1d504222
Pasi Hietanen <pasi.hietanen@protonmail.com>
2019-07-04 11:18:18 +0300

    prettier

--- config/db.js
+++ config/db.js
@@ -3,18 +3,18 @@ const config = require('config');
 const db = config.get('mongoURI');
 
 const connectDB = async () => {
-	try {
-		await mongoose.connect(db, {
-			useNewUrlParser: true,
-			useCreateIndex: true,
-			useFindAndModify: false,
-		});
-		console.log('MongoDB connected...');
-	} catch (err) {
-		console.error(err.message);
-		// exit process with failure!
-		process.exit(1);
-	}
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    });
+    console.log('MongoDB connected...');
+  } catch (err) {
+    console.error(err.message);
+    // exit process with failure!
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
--- routes/api/events.js
+++ routes/api/events.js
@@ -1,80 +1,80 @@
 const express = require('express');
 const config = require('config');
 const router = express.Router();
-const {check, validationResult} = require('express-validator');
+const { check, validationResult } = require('express-validator');
 const Event = require('../../models/Events.js');
 
 // POST event
 
 router.post(
-	'/',
-	[
-		check('title', 'Title is required')
-			.not()
-			.isEmpty(),
-		check('location', 'Location is required')
-			.not()
-			.isEmpty(),
-		check('from', 'Date is required')
-			.not()
-			.isEmpty(),
-	],
-	async (req, res) => {
-		const errors = validationResult(req);
-		if (!errors.isEmpty()) {
-			return res.status(400).json({errors: errors.array()});
-		}
+  '/',
+  [
+    check('title', 'Title is required')
+      .not()
+      .isEmpty(),
+    check('location', 'Location is required')
+      .not()
+      .isEmpty(),
+    check('from', 'Date is required')
+      .not()
+      .isEmpty()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-		try {
-			const newEvent = new Event({
-				title: req.body.title,
-				description: req.body.description,
-				location: req.body.location,
-				from: req.body.from,
-				to: req.body.to,
-				category: req.body.category,
-				teams: req.body.teams,
-			});
+    try {
+      const newEvent = new Event({
+        title: req.body.title,
+        description: req.body.description,
+        location: req.body.location,
+        from: req.body.from,
+        to: req.body.to,
+        category: req.body.category,
+        teams: req.body.teams
+      });
 
-			const event = await newEvent.save();
-			console.log('event saved!');
-			res.json(event);
-		} catch (err) {
-			console.error(err.message);
-			res.status(500).send('Server Error');
-		}
-	},
+      const event = await newEvent.save();
+      console.log('event saved!');
+      res.json(event);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
 );
 
 // GET event by id
 
 router.get('/:id', async (req, res) => {
-	try {
-		const event = await Event.findById(req.params.id);
+  try {
+    const event = await Event.findById(req.params.id);
 
-		if (!event) {
-			return res.status(404).json({msg: 'Event not found'});
-		}
-		res.json(event);
-	} catch (err) {
-		console.error(err.message);
-		if (err.kind === 'ObjectId') {
-			return res.status(404).json({msg: 'Event not found'});
-		}
-		res.status(500).send('Server Error');
-	}
+    if (!event) {
+      return res.status(404).json({ msg: 'Event not found' });
+    }
+    res.json(event);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Event not found' });
+    }
+    res.status(500).send('Server Error');
+  }
 });
 
 // GET all events
 
 router.get('/', async (req, res) => {
-	try {
-		const events = await Event.find();
-		res.json(events);
-	} catch (err) {
-		console.error(err.message);
-		res.status(500).send('Server Error');
-	}
+  try {
+    const events = await Event.find();
+    res.json(events);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 module.exports = router;
