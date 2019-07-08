ad59e46e9426fdcc92e1e4f802d061916f308760
Samuel Koivisto <samuel.j.koivisto@gmail.com>
2019-07-08 12:12:21 +0300

    login route

---
+++ models/User.js
@@ -0,0 +1,23 @@
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  admin: {
+    type: Boolean,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
