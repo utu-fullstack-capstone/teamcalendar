5fc6a88240a4eab3a4498ff70a553df3bd756f13
Samuel Koivisto <samuel.j.koivisto@gmail.com>
2019-07-18 12:19:59 +0300

    actions

---
+++ client/src/actions/types.js
@@ -0,0 +1,8 @@
+// authentication actions
+export const LOG_OUT = 'LOG_OUT';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
+export const USER_LOADED = 'USER_LOADED';
+export const ADMIN_LOADED = 'ADMIN_LOADED';
+export const AUTH_ERROR = 'AUTH_ERROR';
+export const ACCOUNT_DELETED = 'ACCOUNT_DELETED';
