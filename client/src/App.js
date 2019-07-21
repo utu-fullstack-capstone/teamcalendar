41c40bb332b7cd4d8dfd375ee9e9ee67b3ebfa7e
Samuel Koivisto <samuel.j.koivisto@gmail.com>
2019-07-21 11:45:25 +0300

    fixing App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { useEffect, Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from './components/Navbar';
 import Feed from './components/Feed';
@@ -9,31 +9,38 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { setToken } from './actions/login';
+import { loadUser } from './actions/login.js';
+import store from './store';
 import './App.css';
 
 if (localStorage.token) {
   setToken(localStorage.token);
 }
 
-const App = () => (
-  <Router>
-    <Fragment>
-      <NavBar />
-      <Container>
-        <Row>
-          <Col>
-            <Route exact path="/" component={Feed} />
-            <Switch>
-              <Route exact path="/feed" component={Feed} />
-              <Route exact path="/calendar" component={Calendar} />
-              <Route exact path="/settings" component={Settings} />
-              <Route exact path="/login" component={Login} />
-            </Switch>
-          </Col>
-        </Row>
-      </Container>
-    </Fragment>
-  </Router>
-);
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+  return (
+    <Router>
+      <Fragment>
+        <NavBar />
+        <Container>
+          <Row>
+            <Col>
+              <Route exact path="/" component={Feed} />
+              <Switch>
+                <Route exact path="/feed" component={Feed} />
+                <Route exact path="/calendar" component={Calendar} />
+                <Route exact path="/settings" component={Settings} />
+                <Route exact path="/login" component={Login} />
+              </Switch>
+            </Col>
+          </Row>
+        </Container>
+      </Fragment>
+    </Router>
+  );
+};
 
 export default App;
