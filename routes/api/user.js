303b7f072df99e62d5404fda7d734375103dc11c
Samuel Koivisto <samuel.j.koivisto@gmail.com>
2019-08-07 14:21:38 +0300

    aws ses

--- routes/api/user.js
+++ routes/api/user.js
@@ -122,6 +122,86 @@ router.post(
 
         await user.save();
         console.log('User saved');
+
+        var aws = require('aws-sdk');
+
+        // Provide the full path to your config.json file.
+        aws.config.loadFromPath('./../config/default.json');
+
+        // Replace sender@example.com with your "From" address.
+        // This address must be verified with Amazon SES.
+        const sender = 'Sender Name <capstonegofore@gmail.com>';
+
+        // Replace recipient@example.com with a "To" address. If your account
+        // is still in the sandbox, this address must be verified.
+        const recipient = 'success@simulator.amazonses.com';
+
+        // Specify a configuration set. If you do not want to use a configuration
+        // set, comment the following variable, and the
+        // ConfigurationSetName : configuration_set argument below.
+        const configuration_set = 'ConfigSet';
+
+        // The subject line for the email.
+        const subject = 'Käyttäjätunnuksesi';
+
+        // The email body for recipients with non-HTML email clients.
+        const body_text =
+          'Käyttäjätunnuksesi ja salasanasi Positive Impact Portaliin\r\n' +
+          `Käyttäjätunnus: ${email}\r\n` +
+          `Salasana: ${password}`;
+
+        // The HTML body of the email.
+        const body_html = `<html>
+<head></head>
+<body>
+  <h1>Amazon SES Test (SDK for JavaScript in Node.js)</h1>
+  <p>This email was sent with
+    <a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the
+    <a href='https://aws.amazon.com/sdk-for-node-js/'>
+      AWS SDK for JavaScript in Node.js</a>.</p>
+</body>
+</html>`;
+
+        // The character encoding for the email.
+        const charset = 'UTF-8';
+
+        // Create a new SES object.
+        var ses = new aws.SES();
+
+        // Specify the parameters to pass to the API.
+        var params = {
+          Source: sender,
+          Destination: {
+            ToAddresses: [recipient]
+          },
+          Message: {
+            Subject: {
+              Data: subject,
+              Charset: charset
+            },
+            Body: {
+              Text: {
+                Data: body_text,
+                Charset: charset
+              },
+              Html: {
+                Data: body_html,
+                Charset: charset
+              }
+            }
+          },
+          ConfigurationSetName: configuration_set
+        };
+
+        //Try to send the email.
+        ses.sendEmail(params, function(err, data) {
+          // If something goes wrong, print an error message.
+          if (err) {
+            console.log('err.message', err.message);
+          } else {
+            console.log('Email sent! Message ID: ', data.MessageId);
+          }
+        });
       } else {
         return res.status(401).json({ msg: 'Access denied' });
       }
