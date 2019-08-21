133076b92397311771c84f466c459f56e61a6401
Samuel Koivisto <samuel.j.koivisto@gmail.com>
2019-08-21 10:21:20 +0300

    profile fix

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -4,12 +4,14 @@ import axios from 'axios';
 import store from '../store';
 
 const Profile = () => {
-  const [profile, setProfile] = useState();
+  const [profile, setProfile] = useState({ name: '', email: '', password: '' });
   useEffect(() => {
     const user = store.getState().loginReducer.user;
     const fetchProfile = async () => {
-      const account = await axios.get(`/api/user/${user._id}`);
-      setProfile(account.data);
+      if (user._id) {
+        const account = await axios.get(`/api/user/me/${user._id}`);
+        setProfile(account.data);
+      }
     };
     fetchProfile();
   }, []);
@@ -17,7 +19,7 @@ const Profile = () => {
   return (
     <>
       <h3>Profiili</h3>
-      Nimi: {profile.name}
+      Nimi:
     </>
   );
 };
